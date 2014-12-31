1f680342e67662829156fa1238a2f2020597722f
diaodou <luotianbao@gmail.com>
2014-12-31 23:33:44 +0800

    解决依赖问题

--- src/main/scala/org/hyper/data/service/api/ApiServer.scala
+++ src/main/scala/org/hyper/data/service/api/ApiServer.scala
@@ -1,16 +1,19 @@
 package org.hyper.data.service.api
 
+import io.undertow.Undertow
+import io.undertow.server.{HttpServerExchange, HttpHandler}
+import io.undertow.util.Headers
+
 object ApiServer {
   def main(args: Array[String]): Unit = {
-    val server = Undertow.builder()
-      .addHttpListener(8080, "localhost")
-      .setHandler(new HttpHandler() {
-      @Override
-      public void handleRequest(final HttpServerExchange exchange) throws Exception {
-        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-        exchange.getResponseSender().send("Hello World");
+    val server = Undertow.builder
+      .addHttpListener(8080, "0.0.0.0")
+      .setHandler(new HttpHandler {
+      override def handleRequest(exchange: HttpServerExchange): Unit = {
+        exchange.getResponseHeaders.put(Headers.CONTENT_TYPE, "text/plain")
+        exchange.getResponseSender.send("Hello World")
       }
-    }).build();
-    server.start();
+    }).build()
+    server.start
   }
 }
